8ee34ef71f1841843cbbbb37209c69242632cc7c
Iván Herrera González <ivanherreragonza@gmail.com>
2026-05-05 16:01:18 +0200

    fixes para el deploy

--- vite.config.js
+++ vite.config.js
@@ -5,24 +5,6 @@ export default defineConfig({
   base: '/',
   plugins: [react()],
   build: {
-    rollupOptions: {
-      output: {
-        manualChunks: (id) => {
-          if (id.includes('node_modules/react')) {
-            return 'react-vendor'
-          }
-          if (id.includes('node_modules/framer-motion')) {
-            return 'framer-motion'
-          }
-          if (id.includes('node_modules/recharts')) {
-            return 'recharts'
-          }
-          if (id.includes('node_modules/maplibre-gl')) {
-            return 'maplibre'
-          }
-        }
-      }
-    },
     chunkSizeWarningLimit: 1000,
   }
 })
